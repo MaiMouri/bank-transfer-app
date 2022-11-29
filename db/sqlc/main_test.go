f4f5ee5ec0b8f18ca4fa8bca10c4ff97d31f8cae
毛利 舞 <mori-ma@MACN0048.local>
2022-11-29 19:02:02 +0900

    first commit

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("can not to db", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
