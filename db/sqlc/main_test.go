698a21bef18d58ff007a16a1bd4dc5ecc7f82693
MouriMai <mori-ma@ibjapan.jp>
2023-01-01 15:50:15 +0900

    deadlock simulation and how to fix

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -9,21 +9,23 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var testQueries *Queries
-var testDB *sql.DB
-
 const (
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
+var testQueries *Queries
+var testDB *sql.DB
+
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("can not to db", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
